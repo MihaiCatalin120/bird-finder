cfbf8912a79235df66f761a213640499e80c6507
Mihai Catalin Toderita <mihaicatalin449@gmail.com>
2022-05-23 19:22:31 +0300

    Base structure for client 💪

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createTheme, ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import PageRoutes from "./pages/main";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const theme = createTheme({
+		palette: {
+      text: {
+        primary: "#fff",
+        secondary: "#ddd"
+      },
+      background: { paper: "#000" }
+		}
+	});
+
+  return <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <PageRoutes />
+    </BrowserRouter>
+  </ThemeProvider>;
+
 }
 
 export default App;
---
+++ client/src/pages/main/main-view/index.tsx
@@ -0,0 +1,10 @@
+import { observer } from "mobx-react";
+
+
+const MainPage= () => {
+    return <div>
+        Hi
+    </div>
+}
+
+export default observer(MainPage)
